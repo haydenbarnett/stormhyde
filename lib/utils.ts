f76980815fd6a45cdbb12c7c9dbb9f518a221eb3
Hayden Barnett <hello@haydenbarnett.com>
2024-04-16 21:36:51 +1000

    Custom meta description

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,6 +4,7 @@ import { site } from '@/data/site';
 export const getProductMetadata = (product: ProductData) => {
   return {
     title: `${product.title} | ${site.name}`,
+    description: product.description,
     openGraph: {
       images: [product.thumbnail],
     },
